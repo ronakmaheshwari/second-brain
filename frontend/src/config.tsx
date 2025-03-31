69b3cbb4fe725c6897ebddfffb0afac621af8383
ronakmaheshwari077 <ronakmaheshwari077@google.com>
2025-03-31 12:56:35 +0530

    Added AWS

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,1 +1,2 @@
-export const Backend_Url = "http://localhost:3000/api/v1"
+// export const Backend_Url = "http://localhost:3000/api/v1"
+export const Backend_Url = "ec2-65-2-123-205.ap-south-1.compute.amazonaws.com/api/v1"
