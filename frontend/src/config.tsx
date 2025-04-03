2517d550e90948bea7f5dd1cf945cee0f06b8251
ronakmaheshwari077 <ronakmaheshwari077@google.com>
2025-04-03 19:45:03 +0530

    Added Render Backend

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,2 +1,3 @@
-export const Backend_Url = "http://localhost:3000/api/v1"
+// export const Backend_Url = "http://localhost:3000/api/v1"
 // export const Backend_Url = "ec2-65-2-123-205.ap-south-1.compute.amazonaws.com:8080/api/v1"
+export const Backend_Url = "https://second-brain-7f90.onrender.com/api/v1"
